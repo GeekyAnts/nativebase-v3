61269313038ecf29eafc1067cd3f8fed9c2b4f96
Ankur Kedia <theankurkedia@gmail.com>
2020-10-12 16:25:42 +0530

    Added addons for example

--- example/src/storybook/stories/components/primitives/Switch.tsx
+++ example/src/storybook/stories/components/primitives/Switch.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import { VStack, Switch } from 'native-base';
+import { select, boolean } from '@storybook/addon-knobs';
 
 export default function () {
   return (
     <VStack space={50}>
-    <Switch
-      size="sm"
-      // isInvalid
-      // onColor="seagreen"
-      // offColor="red"
-      // switchTrackColor="#f8d7d9"
+      <Switch
+        onColor="seagreen"
+        // isChecked={boolean(
+        //   'isChecked',
+        //   false
+        // )}
+      size={select(
+        'size',
+        ['sm', 'md', 'lg'],
+        'sm'
+      )}
+      isDisabled={boolean(
+        'isDisabled',
+        false
+      )}
+      isInvalid={boolean(
+        'isInvalid',
+        false
+      )}
     />
     <Switch
       size="md"
-      // isInvalid
       onColor="seagreen"
       offColor="red"
-      // switchTrackColor="#f8d7d9"
-    />
+      name="switch-sample"
+      onToggle={(val: any) => console.log("button toggle value", val)}
+      iosBgColor="#f8d7d9"
+      />
     <Switch
       size="lg"
-      // isInvalid
       onColor="seagreen"
       offColor="red"
+      defaultIsChecked={true}
       // switchTrackColor="#f8d7d9"
     />
     </VStack>
