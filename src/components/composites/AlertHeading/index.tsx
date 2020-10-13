ad224bd51558faf977e77ad14443205f7fd83170
Ankur Kedia <theankurkedia@gmail.com>
2020-10-13 19:02:51 +0530

    Added Heading and updated stories

--- src/components/composites/AlertHeading/index.tsx
+++ src/components/composites/AlertHeading/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Heading, IHeadingProps } from '../../primitives';
+import React from "react";
+import { Heading, IHeadingProps } from "../../primitives";
 
 const AlertHeading = ({ style, ...props }: IHeadingProps) => {
-  return <Heading mr="2" headerSize="md" {...props} style={style} />;
+  return <Heading mr="2" size="md" {...props} style={style} />;
 };
 
 export default AlertHeading;
