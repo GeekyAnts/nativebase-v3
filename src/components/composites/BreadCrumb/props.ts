2340c7a9aacb1fb50feb9565c33db7b599c39dfd
MD-REHMAN <Rehman.2468@gmail.com>
2020-11-04 15:59:31 +0530

    fix: fixed decaration conflict

--- src/components/composites/BreadCrumb/props.ts
+++ src/components/composites/BreadCrumb/props.ts
@@ -1,21 +1,5 @@
-import type { ViewProps, ViewStyle } from 'react-native';
-import type {
-  BorderProps,
-  ColorProps,
-  FlexboxProps,
-  LayoutProps,
-  SpaceProps,
-} from 'styled-system';
-import type {
-  customBorderProps,
-  customBackgroundProps,
-  customOutlineProps,
-  customLayoutProps,
-  customExtraProps,
-  customShadowProps,
-} from '../../../utils/customProps';
+import type { ViewStyle } from 'react-native';
 
-// @ts-ignore
 import type { IBoxProps, ITextProps, IFlexProps } from '../../primitives';
 
 export type IBreadCrumbProps = IBoxProps &
@@ -25,19 +9,4 @@ export type IBreadCrumbProps = IBoxProps &
     children?: JSX.Element[] | JSX.Element | any;
     spacing?: number;
     separator?: string | JSX.Element | JSX.Element[];
-  };
-export type ITextProps = ViewProps &
-  ColorProps &
-  SpaceProps &
-  LayoutProps &
-  FlexboxProps &
-  customBorderProps &
-  customExtraProps &
-  customOutlineProps &
-  customShadowProps &
-  customLayoutProps &
-  customBackgroundProps &
-  BorderProps & {
-    style?: ViewStyle;
-    children?: string;
   };
