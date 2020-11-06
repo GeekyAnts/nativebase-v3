a74c43aeeebe638dfa2e186f8d63491519129002
Ankur Kedia <theankurkedia@gmail.com>
2020-11-06 16:59:03 +0530

    Updated example for spinner

--- example/src/storybook/stories/components/primitives/Spinner/color.tsx
+++ example/src/storybook/stories/components/primitives/Spinner/color.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Spinner, View, Inline } from 'native-base';
+import { Spinner, HStack } from 'native-base';
 
 export default function () {
   return (
-    <View display="flex" justifyContent="space-between" alignItems="center">
-      <Inline>
-        <Spinner m={2} color="danger.5" />
-        <Spinner m={2} color="green.5" />
-        <Spinner m={2} color="blue.5" />
-        <Spinner m={2} color="warning.1" />
-      </Inline>
-    </View>
+    <HStack space={2}>
+      <Spinner color="danger.5" />
+      <Spinner color="green.5" />
+      <Spinner color="blue.5" />
+      <Spinner color="warning.1" />
+    </HStack>
   );
 }
--- example/src/storybook/stories/components/primitives/Spinner/size.tsx
+++ example/src/storybook/stories/components/primitives/Spinner/size.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Spinner, View, Inline } from 'native-base';
+import { Spinner, HStack } from 'native-base';
 
 export default function () {
   return (
-    <View display="flex" justifyContent="space-between" alignItems="center">
-      <Inline mb={2} alignItems="center">
-        <Spinner mr={5} size="sm" />
-        <Spinner size="lg" />
-      </Inline>
-    </View>
+    <HStack space={3}>
+      <Spinner size="sm" />
+      <Spinner size="lg" />
+    </HStack>
   );
 }
