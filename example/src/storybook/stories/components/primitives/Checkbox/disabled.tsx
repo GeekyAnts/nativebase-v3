01125af8b905cdda9ae4c9887f4c10f79242ca4d
Ankur Kedia <theankurkedia@gmail.com>
2020-11-09 11:51:12 +0530

    Removed inline component

--- example/src/storybook/stories/components/primitives/Checkbox/disabled.tsx
+++ example/src/storybook/stories/components/primitives/Checkbox/disabled.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Checkbox, Inline, Text } from 'native-base';
+import { Checkbox, HStack, Text } from 'native-base';
 
 export default function () {
   return (
-    <Inline>
+    <HStack>
       <Checkbox isDisabled>
         <Text m={2}>Checkbox</Text>
       </Checkbox>
       <Checkbox isDisabled defaultIsChecked>
         <Text m={2}>Checkbox</Text>
       </Checkbox>
-    </Inline>
+    </HStack>
   );
 }
