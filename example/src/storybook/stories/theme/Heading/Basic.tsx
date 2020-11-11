4838c02975b351472ec2d239757c7920bf1c43f2
Ankur Kedia <theankurkedia@gmail.com>
2020-11-11 14:25:09 +0530

    Added extendTheme and its example

---
+++ example/src/storybook/stories/theme/Heading/Basic.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Heading, VStack } from '@native-base/v3';
+
+export default function () {
+  return (
+    <VStack>
+      <Heading> I'm a Heading</Heading>
+      <Heading color="custom.3"> I'm a Heading</Heading>
+    </VStack>
+  );
+}
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,2 @@
 export * from './NativeBaseProvider';
+export * from './extendTheme';
