1ad72db3de478208b695b0180320d1e72de7b9e4
Rohit Singh <rohits@geekyants.com>
2020-11-19 14:58:06 +0530

    feat: rnw in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,31 @@
-import StorybookUIRoot from './storybook';
+// import StorybookUIRoot from './storybook';
 
-export default StorybookUIRoot;
+// export default StorybookUIRoot;
+import React from 'react';
+// import { Box } from "@native-base/v3";
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+
+const { height } = Dimensions.get('screen');
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.center}>
+        <Text>Hello React Native Web!!!</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
--- example/web/webpack.config.js
+++ example/web/webpack.config.js
@@ -87,6 +87,7 @@ module.exports = {
     // This will only alias the exact import "react-native"
     alias: {
       'react-native$': 'react-native-web',
+      '@storybook/react-native': '@storybook/react',
     },
     // If you're working on a multi-platform React Native app, web-specific
     // module implementations should be written in files using the extension
