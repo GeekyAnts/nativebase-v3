7830a1c37cc039b5ec191c6e21d8c283d9ccbe26
Ankur Kedia <theankurkedia@gmail.com>
2020-11-20 15:54:00 +0530

    Added useMediaQuery

--- example/src/storybook/stories/hooks/useMediaQuery/max-height.tsx
+++ example/src/storybook/stories/hooks/useMediaQuery/max-height.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, useMediaQuery } from '@native-base/v3';
 
 export default function () {
-  const [isSmaller] = useMediaQuery([{ maxHeight: 512 }]);
+  const [isSmaller] = useMediaQuery({ maxHeight: 512 });
   return (
     <Text>
       {isSmaller ? 'Height smaller than 512' : 'Height larger than 512'}
--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -14,33 +14,59 @@ type SubQuery = {
 type Query = Array<SubQuery>;
 
 export function useMediaQuery(query: SubQuery | Query) {
-  if (Array.isArray(query)) {
-    return query.map((subQuery: SubQuery) => executeQuery(subQuery));
-  } else {
-    return [executeQuery(query)];
-  }
+  const height = useWindowDimensions()?.height;
+  const width = useWindowDimensions()?.width;
+  return iterateQuery(query, height, width);
 }
 
-function executeQuery(query: SubQuery) {
-  let key = Object.keys(query)[0];
-  let val = Object.values(query)[0];
+function iterateQuery(
+  query: SubQuery | Query,
+  height?: number,
+  width?: number
+) {
+  let key;
+  let val;
+  let queryResults = [];
+  if (Array.isArray(query)) {
+    query.forEach((subQuery: SubQuery) => {
+      key = Object.keys(subQuery)[0];
+      val = Object.values(subQuery)[0];
+      queryResults.push(calculateQuery(key, val, height, width));
+    });
+  } else {
+    key = Object.keys(query)[0];
+    val = Object.values(query)[0];
+    queryResults.push(calculateQuery(key, val, height, width));
+  }
+  return queryResults;
+}
+
+function calculateQuery(
+  key: string,
+  val?: number | string,
+  height?: number,
+  width?: number
+) {
   let retval;
+  if (isNil(width) || isNil(height) || isNil(val)) {
+    return;
+  }
   switch (key) {
     case 'maxWidth':
-      retval = !isNil(val) ? useWindowDimensions()?.width <= val : undefined;
+      retval = !isNil(val) ? width <= val : undefined;
       break;
     case 'minWidth':
-      retval = !isNil(val) ? useWindowDimensions()?.width >= val : undefined;
+      retval = !isNil(val) ? width >= val : undefined;
       break;
     case 'maxHeight':
-      retval = !isNil(val) ? useWindowDimensions()?.height <= val : undefined;
+      retval = !isNil(val) ? height <= val : undefined;
       break;
     case 'minHeight':
-      retval = !isNil(val) ? useWindowDimensions()?.height >= val : undefined;
+      retval = !isNil(val) ? height >= val : undefined;
       break;
     case 'orientation':
       if (!isNil(val)) {
-        if (useWindowDimensions()?.width > useWindowDimensions()?.height) {
+        if (width > height) {
           retval = val === 'landscape';
         } else {
           retval = val === 'portrait';
