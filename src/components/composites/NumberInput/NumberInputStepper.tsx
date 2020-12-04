9ce48d4e4bfbfb3f1009923f0304061acf77353f
MD-REHMAN <Rehman.2468@gmail.com>
2020-12-04 11:28:32 +0530

    fix: merged increment and decrement code

--- src/components/composites/NumberInput/NumberInputStepper.tsx
+++ src/components/composites/NumberInput/NumberInputStepper.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
-import { VStack } from '../../primitives';
+import { VStack, Box, Icon } from '../../primitives';
 import { Divider } from '../../composites';
-import type { INumberInputFieldProps } from './props';
+import { usePropsConfig } from '../../../theme';
+import type { INumberInputSteppersProps } from './index';
+import { TouchableOpacity } from 'react-native';
 
-const NumberInputStepper = ({ children, ...props }: INumberInputFieldProps) => {
+export const NBStepper = ({ children, ...props }: any) => {
+  const {
+    style,
+    isIncrement,
+    disablitityCheck,
+    _active,
+    _disabled,
+    isDisabled,
+    ariaLabel,
+    pressHandler,
+    ...newProps
+  } = usePropsConfig('NumberInputStepper', props);
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      disabled={disablitityCheck || isDisabled}
+      onPress={pressHandler}
+      accessible
+      accessibilityLabel={ariaLabel}
+    >
+      <Box
+        {...newProps}
+        {..._active}
+        {...(disablitityCheck || isDisabled ? _disabled : {})}
+        style={style}
+      >
+        {children || isIncrement ? (
+          <Icon name="arrow-drop-up" type="MaterialIcons" />
+        ) : (
+          <Icon name="arrow-drop-up" type="MaterialIcons" />
+        )}
+      </Box>
+    </TouchableOpacity>
+  );
+};
+
+const NumberInputStepper = ({
+  children,
+  ...props
+}: INumberInputSteppersProps) => {
   return (
     <VStack divider={<Divider />} {...props}>
       {children}
