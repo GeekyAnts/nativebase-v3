4e7b544091130216456e23300cb44b698e01de0a
Rohit Singh <rohits@geekyants.com>
2020-12-07 19:23:05 +0530

    chore: added path to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@native-base/v3': './src/index',
+          '@nativebase/Box': './src/components/primitives/Box/index',
+          '@native-base/v3/Accordion':
+            './src/components/composites/Accordion/index',
+        },
+      },
+    ],
+  ],
 };
