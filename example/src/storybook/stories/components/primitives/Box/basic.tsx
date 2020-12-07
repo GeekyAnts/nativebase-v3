acc0470245fcb502f060604f481519f702c49218
Ankur Kedia <theankurkedia@gmail.com>
2020-12-07 20:32:53 +0530

    Changes in tsconfig

--- example/src/storybook/stories/components/primitives/Box/basic.tsx
+++ example/src/storybook/stories/components/primitives/Box/basic.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@native-base/v3';
 export const Basic = () => {
   return (
-    <Box width="100%" bg="orange.300" p={4} color="white" shadow={4}>
+    <Box width="100%" bg="orange.300" p={4} shadow={4}>
       This is a Box
     </Box>
   );
