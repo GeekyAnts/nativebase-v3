700bfcc26661ffb074907ed9d4af1b92273c6711
Nishan <nishanbende@gmail.com>
2020-12-09 18:30:26 +0530

    feat: accessibility announcement in snackbar

--- src/components/composites/Snackbar/props.tsx
+++ src/components/composites/Snackbar/props.tsx
@@ -1,11 +1,14 @@
 import type { ISlideProps } from '../Transitions';
 
-export type ISnackbarProps = ISlideProps & {};
+export type ISnackbarProps = ISlideProps & {
+  accessibilityAnnouncement?: string;
+};
 export type IsetSnackbar = (
   component: React.ReactNode,
   config?: ISnackbarProps & {
     enableOverlay?: boolean;
     duration?: number;
+    accessibilityAnnouncement?: string;
   }
 ) => void;
 export type IuseSnackbarProps = () => {
